f644921dc8ec4175b9b1e6d6469048de1fd72ebc
Oleg Kovylin <oak.rs.school@gmail.com>
2022-07-31 18:53:05 +0300

    add: schema.prisma & migration

---
+++ prisma/migrations/20220731154302_init/migration.sql
@@ -0,0 +1,69 @@
+-- CreateTable
+CREATE TABLE "artists" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "grammy" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "artists_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "albums" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "artistId" TEXT,
+
+    CONSTRAINT "albums_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tracks" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "artistId" TEXT,
+    "albumId" TEXT,
+    "duration" INTEGER NOT NULL,
+
+    CONSTRAINT "tracks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "version" INTEGER NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "favorites" (
+    "id" TEXT NOT NULL,
+    "albumsIds" TEXT[],
+    "artistsIds" TEXT[],
+    "tracksIds" TEXT[],
+
+    CONSTRAINT "favorites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "albums_artistId_key" ON "albums"("artistId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tracks_artistId_key" ON "tracks"("artistId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tracks_albumId_key" ON "tracks"("albumId");
+
+-- AddForeignKey
+ALTER TABLE "albums" ADD CONSTRAINT "albums_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "artists"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tracks" ADD CONSTRAINT "tracks_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "artists"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tracks" ADD CONSTRAINT "tracks_albumId_fkey" FOREIGN KEY ("albumId") REFERENCES "albums"("id") ON DELETE SET NULL ON UPDATE CASCADE;
