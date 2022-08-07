0e66ae306609de50f5fe505ed98331c841dd7681
Oleg Kovylin <oak.rs.school@gmail.com>
2022-08-07 23:41:43 +0300

    ref: strategy

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -13,6 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: any) {
-    return { login: payload.login, password: payload.password };
+    return { id: payload.id, login: payload.login };
   }
 }
